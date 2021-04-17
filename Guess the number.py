33cce536d768aa7e8a08baa23e65677d36ef1e94
Gaenox <81582956+Gaenox@users.noreply.github.com>
2021-04-17 12:34:20 +0200

    Update and rename import random.py to Guess the number.py

--- Guess the number.py
+++ Guess the number.py
@@ -1,7 +1,7 @@
 import random
+import time
 print('''Let's play a game called guess the number in my mind (1-1000)''')
 rando = random.randint(1, 1001)
-print(rando)
 while True:
     x = int(input('''Enter the number I'm guessing:'''))
     if x > rando:
@@ -10,4 +10,5 @@ while True:
         print('a little higher')
     else:
         print('You found it!')
+        time.sleep(1)
         break
